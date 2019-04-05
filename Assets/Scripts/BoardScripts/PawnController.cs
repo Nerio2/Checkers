d373d83e4d67bc0668e8ce65e32c237699e7ee74
Patryk Kozarski <Patryknerio2@gmail.com>
2019-04-05 21:35:36 +0200

    queen init v3

--- Assets/Scripts/BoardScripts/PawnController.cs
+++ Assets/Scripts/BoardScripts/PawnController.cs
@@ -8,15 +8,18 @@ public class PawnController : MonoBehaviour {
 	public bool queen;
 	public Sprite Defult;
 	public Sprite Queen;
-	 // Start is called before the first frame update
-	 void Start() {
+	// Start is called before the first frame update
+	void Start() {
 		queen = false;
 	}
 
 	// Update is called once per frame
 	void Update() {
+		if ( ( player == 1 && transform.position.y == -7 ) || ( player == 0 && transform.position.y == 7 ) )
+			queen = true;
 		if ( queen )
 			GetComponent<SpriteRenderer>().sprite = Queen;
+
 	}
 
 	private void OnMouseDown() {
